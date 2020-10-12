086856fcca6934503b8d1c4a31c7a0cbd06261db
Ting <badonlineqq@gmail.com>
2020-10-12 22:22:20 +1100

    default api url changed

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,6 +1,6 @@
 export function getStrapiURL(path = "") {
   return `${
-    process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337"
+    process.env.NEXT_PUBLIC_STRAPI_API_URL || "https://my-next-backend.herokuapp.com"
   }${path}`;
 }
 
