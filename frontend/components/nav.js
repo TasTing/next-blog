dee04edbdaba534f3acbd514f3b39e55ac672d12
Ting <badonlineqq@gmail.com>
2020-10-20 22:05:59 +1100

    footer nav is now responsive
    bootstrap siteName displayed on navbar

--- frontend/components/nav.js
+++ frontend/components/nav.js
@@ -4,12 +4,13 @@ import Link from "next/link";
 const Nav = ({ categories, title }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">{title}</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
               aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
+
       <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <a className="navbar-brand" href="/">{title}</a>
         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
           <li key={"about"} className="nav-item active">
             <Link as={`/about`} href={`/about`}>
