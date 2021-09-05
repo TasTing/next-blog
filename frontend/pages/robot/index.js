c0b62a6d918dbc3c679cb272c57e42a534da9df3
Tingting <badonlineqq@gmail.com>
2021-09-05 16:19:20 +1000

    ADDED ROBOT APPLICATION ONTO /ROBOT
    NOW ABLE TO BROWSER THROUGH /ROBOT

---
+++ frontend/pages/robot/index.js
@@ -0,0 +1,39 @@
+import ReactMarkdown from "react-markdown";
+import Moment from "react-moment";
+import {fetchAPI} from "../../lib/api";
+import Layout from "../../components/layout";
+import Image from "../../components/image";
+import Seo from "../../components/seo";
+import {getStrapiMedia} from "../../lib/media";
+import BlogFooter from "../../components/blogfooter";
+import App from "../../components/Robot/Robot/src/App";
+import React from "react";
+import Intro from "../../components/intro";
+import GithubProfile from "../../components/github";
+import {YoutubePlayer} from "reactjs-media";
+import ContactForm from "../../components/contactform";
+import {Container} from "@material-ui/core";
+
+const Index = ({ categories, global, homepage }) => {
+    const seo = {};
+
+    return (
+        <Layout categories={categories} global={global}>
+            <Seo seo={seo}/>
+            <Container>
+                <App/>
+            </Container>
+        </Layout>
+    );
+};
+
+export async function getStaticProps() {
+    const categories = await fetchAPI("/categories");
+    const homepage = await fetchAPI("/homepage");
+
+    return {
+        props: { categories, homepage },
+        revalidate: 1
+    };
+}
+export default Index;
